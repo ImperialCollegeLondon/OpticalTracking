818cb016aa43b55c93bf05de6f12d5775395f9bc
JJ <jonathanjamesholt@gmail.com>
2026-03-29 00:29:48 +0000

    Feat: Visualise tracker position

--- lib/Camera/Tracker.m
+++ lib/Camera/Tracker.m
@@ -48,6 +48,12 @@ classdef Tracker < handle
             end
             [self.Camera] = deal(camera);
         end
+        function ax = visualise(self)
+            x = self.Tx;
+            y = self.Ty;
+            z = self.Tz;
+            ax = scatter3(x,y,z);
+        end
         function self = add_labels(self, labels)
             names = {self.Name};
             for i = 1:numel(names)
