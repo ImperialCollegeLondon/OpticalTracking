5c7c7740a1ccb943659b1bb3301716559d5b26c1
JJ <jonathanjamesholt@gmail.com>
2026-05-30 20:02:01 +0100

    Plots centre of rotation semi-manually

--- lib/JCS/+Knee/calculate_transforms.m
+++ lib/JCS/+Knee/calculate_transforms.m
@@ -36,15 +36,8 @@ function [transforms, bones] = calculate_transforms(trackers, loading_condition,
     gTti = pagemtimes(gTtti, ttTtc);%multiply here instead of divide in Pam's method
     fTt = pagemldivide(gTfi, gTti); % Transformation of Tibia relative to the femur
 
-    if isempty(ttTtscc)
-        gTtsi = []; 
-        fTts = [];
-        tTts = [];
-    else
-        gTtsi = pagemtimes(gTtti, ttTtscc); % Tibial surface relative to global
-        fTts = pagemldivide(gTfi, gTtsi);
-        tTts = pagemldivide(gTti, gTtsi); %% <<<<<<<<< This makes it constant.
-    end
+
+    fTts = pagemtimes(fTt , tTts);
 
     if isempty(tTts)
         fTts = [];
