b469257694f9ad0972150b2afdcca1bdd9312983
JJ <jonathanjamesholt@gmail.com>
2026-06-18 19:10:58 +0100

    now outputs strays

--- lib/Camera/polaris.m
+++ lib/Camera/polaris.m
@@ -6,8 +6,8 @@ function [trackers, strays] = polaris(raw_headers, lines)
     
 
     passive_strays_idx = find(strcmp(raw_headers, 'Passive Strays'), 1);
-    has_passives = ~isempty(passive_strays_idx);
-    if has_passives
+    has_strays = ~isempty(passive_strays_idx);
+    if has_strays
         idx_q0 = idx_q0(idx_q0 < passive_strays_idx);
 
         stray_headers = raw_headers(passive_strays_idx: end);
@@ -30,7 +30,7 @@ function [trackers, strays] = polaris(raw_headers, lines)
     n_rows = numel(lines) - 1;
     data_tracker = nan(size(idx, 1), size(idx, 2), n_rows);
 
-    if has_passives
+    if has_strays
         data_stray = nan(size(idx_stray, 1), size(idx_stray, 2), n_rows);
     end
 
@@ -41,7 +41,7 @@ function [trackers, strays] = polaris(raw_headers, lines)
         datum(abs(datum) > 1e20) = nan;
         data_tracker(:, :, n) = datum;
 
-        if has_passives
+        if has_strays
             row_stray = row(passive_strays_idx:end);
             datum_stray = extract_strays(stray_headers, idx_stray, row_stray);
             data_stray(:, :, n) = datum_stray;
@@ -61,7 +61,18 @@ function [trackers, strays] = polaris(raw_headers, lines)
         error = squeeze(data_tracker(n, ERR, :));
         trackers(n) = Tracker(name, q0, qx, qy, qz, tx, ty, tz, error);
     end
+
+    if has_strays
+        for n = 1:size(data_stray, 1)
+            tz = squeeze(data_stray(n, 1, :));
+            ty = squeeze(data_stray(n, 2, :));
+            tx = squeeze(data_stray(n, 3, :));
+
+            strays(n) = PassiveStrays(tx, ty, tz);
+        end
+    else
     strays = [];
+    end
 end
 
 
@@ -72,10 +83,7 @@ function datum = extract_strays(headers, idx, row)
     end
 
     if numel(row) > numel(headers)
-        keyboard
-        new_rows = (numel(row) - numel(headers))/4;
-        last_tz = max(idx(:));
-        extra_idx = last_tz + (4:-1:2);
+        row = row(1:length(headers));
     end
 
     datum = str2double(row(idx));
