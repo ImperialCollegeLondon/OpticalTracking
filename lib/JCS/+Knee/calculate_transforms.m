0c6806e98672ab1ffe9df9697561a1d1cf832d9d
JJ <jonathanjamesholt@gmail.com>
2026-06-21 03:53:51 +0100

    fix: bad merge

--- lib/JCS/+Knee/calculate_transforms.m
+++ lib/JCS/+Knee/calculate_transforms.m
@@ -36,9 +36,6 @@ function [transforms, bones] = calculate_transforms(trackers, loading_condition,
     gTti = pagemtimes(gTtti, ttTtc);%multiply here instead of divide in Pam's method
     fTt = pagemldivide(gTfi, gTti); % Transformation of Tibia relative to the femur
 
-
-    fTts = pagemtimes(fTt , tTts);
-
     if isempty(tTts)
         fTts = [];
     else
